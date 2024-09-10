30a78ef76cb3bc379d2d0ebcfff7dadd53d8c3e7
DxnnySZN <yoondaniel2006@gmail.com>
2024-09-10 00:40:43 -0500

    Codes.cpp is now able to utilize Test_Case.txt 9/10/24
    1. C:\Independent CS\Data-Structures-and-Algorithms\Dictionaries\Problem_Sets\Problem_3> g++ -o Codes Codes.cpp
    2. C:\Independent CS\Data-Structures-and-Algorithms\Dictionaries\Problem_Sets\Problem_3> Get-Content Test_Case.txt | ./Codes.exe

--- Dictionaries/Problem_Sets/Problem_3/Codes.cpp
+++ Dictionaries/Problem_Sets/Problem_3/Codes.cpp
@@ -32,4 +32,24 @@ int main() {
         }
     }
     return 0;
+
+    /* Test_Case: a time (adds "time" to the dictionary with a frequency of 1)
+                  a light (adds "light" to the dictionary with a frequency of 1)
+                  f speed ("speed" is not in the dictionary, so the output is 0)
+                  f time ("time" is in the dictionary with a frequency of 1, so the output is 1)
+                  a time (increments the frequency of "time" by 1. the new frequency of "time" is 2)
+                  a time (increments the frequency of "time" by 1 again. the new frequency of "time" is 3)
+                  a speed (adds "speed" to the dictionary with a frequency of 1)
+                  a speed (increments the frequency of "speed" by 1. the new frequency of "speed" is 2)
+                  f graph ("graph" is not in the dictionary, so the output is 0)
+                  a graph (adds "graph" to the dictionary with a frequency of 1)
+                  f time ("time" is in the dictionary with a frequency of 3, so the output is 3)
+                  a lake (adds "lake" to the dictionary with a frequency of 1)
+                  f lake ("lake" is in the dictionary with a frequency of 1, so the output is 1)
+
+       Output:    0
+                  1
+                  0
+                  3
+                  1 */
 }
