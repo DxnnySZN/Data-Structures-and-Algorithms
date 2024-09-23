20618c353d844c3c74b3b2665b7bf37a6d6b1977
yoondaniel1 <yoondaniel2006@gmail.com>
2024-09-23 08:02:06 -0500

    reverse sequence (problem 3) complete 9/23/24

---
+++ Vectors/Problem_3/Reverse_Sequence.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int size;
+    while(cin >> size){
+        if(size != 0){
+            vector<int> v(size);
+            for(int i = 0; i < size; ++i){
+                cin >> v[i];
+            }
+            for(int j = size - 1; j >= 0; --j){ // reverses the sequence once the elements in the vector have been read
+                if(j == 0){
+                    cout << v[j] << " ";
+                }
+                else{
+                    cout << v[j];
+                }
+            }
+        }
+        else{
+            cout << "there are no elements in the vector" << endl;
+        }
+    }
+    return 0;
+}
