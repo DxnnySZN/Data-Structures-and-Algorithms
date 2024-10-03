973217cdfb137aaaf93a24cda7abf2685e7a4eee
DxnnySZN <yoondaniel2006@gmail.com>
2024-10-02 23:56:48 -0500

    Adjacency_Matrix_Implementation.cpp is now able to utilize Test_Case.txt 10/2/24

--- Algorithms/Graphs/Adjacency_Matrix_Implementation/Adjacency_Matrix_Implementation.cpp
+++ Algorithms/Graphs/Adjacency_Matrix_Implementation/Adjacency_Matrix_Implementation.cpp
@@ -4,12 +4,11 @@
 using namespace std;
 
 int main(){
-    int n = 5;
-    vector<vector<int>> A(n, vector<int>(n)); // n -> row | vector<int>(n) -> column
+    int nodeAmt = 5;
+    vector<vector<int>> A(nodeAmt, vector<int>(nodeAmt)); // nodeAmt -> row | vector<int>(nodeAmt) -> column
     int edgeAmt = 7;
 
     // fill the adjacency matrix
-    cout << "list the edges: " << endl;
     for(int k = 0; k < edgeAmt; ++k){
         int u, v;
         cin >> u >> v;
@@ -17,14 +16,36 @@ int main(){
         A[v][u] = 1;
     }
 
-    for(int i = 0; i < n; ++i){
-        for(int j = 0; j < n; ++j){
+    for(int i = 0; i < nodeAmt; ++i){
+        for(int j = 0; j < nodeAmt; ++j){
             cout << A[i][j] << " ";
         }
         cout << endl;
     }
     return 0;
 
-    /* Test_Case:  
-       Output: */
+    /* Notes:     Adjacency Matrix is a 2D vector/array of size n * n, where n is the number of nodes in a graph
+                  Let the matrix be A[][]. A[i][j] = 1 indicates there is an edge from node i to node j
+    
+       Graph:     0--------------1
+                  |             /| \
+                  |          /   |    \   
+                  |       /      |      2
+                  |    /         |    /
+                  | /            | /
+                  4--------------3
+    
+       Test_Case: 0 1
+                  0 4
+                  1 4
+                  1 3
+                  3 4
+                  1 2
+                  2 3
+
+       Output:    0 1 0 0 1 
+                  1 0 1 1 1
+                  0 1 0 1 0
+                  0 1 1 0 1
+                  1 1 0 1 0 */
 }
