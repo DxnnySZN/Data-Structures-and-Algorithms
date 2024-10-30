ce5abb33c90ac0caf1f0d00711e4b74c980cd2f4
yoondaniel1 <yoondaniel2006@gmail.com>
2024-10-30 08:52:54 -0500

    topo sort implementation complete 10/30/24

---
+++ Algorithms/Graph_Algorithms/Topological_Sort/Topo_Sort_Implementation.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+
+using namespace std;
+
+typedef vector<vector<int>> graph;
+
+list<int> topological_sort(graph& g){
+    int nodes = g.size(); // total # of nodes in graph g
+    vector<int> dag(nodes, 0); // dag: directed acyclic graph | stores in-degrees for each node
+
+    // calculates in-degrees for each node
+    for(int i = 0; i < nodes; ++i){
+        for(int j : g[i]){
+            ++dag[j]; // increment in-degree for each adjacent node
+        }
+    }
+    stack<int> s; // stack to store nodes with zero in-degrees
+
+    // pushes all nodes with zero in-degrees into the stack
+    for(int i = 0; i < nodes; ++i){
+        if(dag[i] == 0){
+            s.push(i); 
+        }
+    }
+
+    list<int> output; // topological ordering
+
+    // processes nodes until stack is empty
+    while(!s.empty()){
+        int i = s.top();
+        s.pop();
+        output.push_back(i); // adds node to output
+
+        // reduces in-degrees of all adjacent nodes
+        for(int j : g[i]){
+            if(--dag[j] == 0){ // if in-degree of node becomes 0, push it to stack
+                s.push(j);
+            }
+        }
+    }
+
+    return output;
+}
+
+int main() {
+    int nodes, edges;
+    cin >> nodes >> edges;
+    graph g(nodes);
+
+    // reads each edge and populates the adjacency list
+    for(int i = 0; i < edges; ++i){
+        int source, destiny;
+        cin >> source >> destiny;
+
+        // source has a direct connection (arrow) pointing to destiny
+        g[source].push_back(destiny); // g[destiny].push_back(source); will not work because we have directed graph
+    }
+
+    list<int> output = topological_sort(g);
+
+    // prints output sequence
+    for(auto& element : output){
+        cout << element << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
