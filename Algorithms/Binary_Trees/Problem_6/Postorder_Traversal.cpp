7c218ddb4d063235efc04428c43de3522f6a88ef
DxnnySZN <yoondaniel2006@gmail.com>
2024-11-14 23:53:54 -0600

    postorder traversal (problem 6) complete 11/14/24

--- Algorithms/Binary_Trees/Problem_6/Postorder_Traversal.cpp
+++ Algorithms/Binary_Trees/Problem_6/Postorder_Traversal.cpp
@@ -21,21 +21,21 @@ public: // all attributes & functions are accessible outside the class
     
 };
 
-vector<int> preorder(Node* node) {
+vector<int> postorder(Node* node) {
     vector<int> result;
 
-    // order of the preorder traversal: visit root, visit left subtree, visit right subtree
+    // order of the postorder traversal: visit left subtree, visit right subtree, visit root
     if (node != nullptr) {
-        // visits root
-        result.push_back(node -> data);
-
         // visits left subtree
-        vector<int> leftSubtree = preorder(node -> left);
+        vector<int> leftSubtree = postorder(node -> left);
         result.insert(result.end(), leftSubtree.begin(), leftSubtree.end()); // concatenates all elements in left subtree vector to the end of result vector
 
         // visits right subtree
-        vector<int> rightSubtree = preorder(node -> right);
+        vector<int> rightSubtree = postorder(node -> right);
         result.insert(result.end(), rightSubtree.begin(), rightSubtree.end()); // concatenates all elements in right subtree vector to the end of result vector
+
+        // visits root
+        result.push_back(node -> data);
     }
 
     return result;
@@ -50,9 +50,9 @@ int main() {
     root -> right -> left = new Node(12);
     root -> right -> right = new Node(20);
 
-    vector<int> preorderTraversal = preorder(root);
-    cout << "preorder traversal: ";
-    for (int value : preorderTraversal) {
+    vector<int> postorderTraversal = postorder(root);
+    cout << "postorder traversal: ";
+    for (int value : postorderTraversal) {
         cout << value << " ";
     }
     cout << endl;
@@ -65,5 +65,5 @@ int main() {
                   / \   / \
                  3   7 12  20
 
-       Output : preorder traversal: 10 5 3 7 15 12 20 */
+       Output : postorder traversal: 3 7 5 12 20 15 10 */
 }
