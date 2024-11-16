3d09ab899cda11d179cbc5638ad870fd1fb0a9bb
DxnnySZN <yoondaniel2006@gmail.com>
2024-11-15 23:56:48 -0600

    commented explanation for inorder traversal (problem 7) 11/15/24

--- Algorithms/Binary_Trees/Problem_7/Inorder_Traversal.cpp
+++ Algorithms/Binary_Trees/Problem_7/Inorder_Traversal.cpp
@@ -58,4 +58,49 @@ int main() {
     cout << endl;
 
     return 0;
+
+    /* Graph:         1
+                     / \
+                    2   3
+                   / \ / \
+                  4  5 6  7
+
+       Output: inorder traversal: 4 2 5 1 6 3 7 
+       
+       Explanation: Start at the root node 1:
+                        Traverse the left subtree rooted at node 2.
+
+                    At node 2:
+                        Traverse the left subtree rooted at node 4.
+
+                    At node 4:
+                        No left child → Visit 4 (add 4 to the result).
+                        No right child → Return to node 2.
+
+                    Back at node 2:
+                        Visit 2 (add 2 to the result).
+                        Traverse the right subtree rooted at node 5.
+
+                    At node 5:
+                        No left child → Visit 5 (add 5 to the result).
+                        No right child → Return to node 1.
+
+                    Back at the root node 1:
+                        Visit 1 (add 1 to the result).
+                        Traverse the right subtree rooted at node 3.
+
+                    At node 3:
+                        Traverse the left subtree rooted at node 6.
+
+                    At node 6:
+                        No left child → Visit 6 (add 6 to the result).
+                        No right child → Return to node 3.
+
+                    Back at node 3:
+                        Visit 3 (add 3 to the result).
+                        Traverse the right subtree rooted at node 7.
+
+                    At node 7:
+                        No left child → Visit 7 (add 7 to the result).
+                        No right child → Return. */
 }
